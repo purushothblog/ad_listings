eb2afdb3726ddf689752b4179c54994a67e99c56
pruzoth <pruzoth@gmail.com>
2014-09-08 17:19:27 +0530

    Associations added

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,10 +1,9 @@
 # encoding: utf-8
- include CarrierWave::RMagick
- require 'carrierwave/orm/activerecord'
+  require 'carrierwave/orm/activerecord'
 class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -34,7 +33,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fit => [200, 200]
+     process :resize_to_fit => [300, 300]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
